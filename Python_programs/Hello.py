a9222b6d54e74558826db4f700f45069bc21ed96
Sivamani321 <sivamani.perftest@gmail.com>
2025-07-26 14:21:39 +0530

    Python program file

---
+++ Python_programs/Hello.py
@@ -0,0 +1,18 @@
+# This is 1.Hello, sivamani
+print("Hello, Sivamani!")
+ # 2nd one
+a = 5
+b = 3
+print("Sum:", a + b)
+# 3rd one
+name = input("Enter your name: ")
+print("Hello,", name)
+# 4th one To Check Even or odd
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("Even number")
+else:
+    print("odd number")
+# 5th one Simple loop
+for i in range(5):
+    print("Number:", i)
